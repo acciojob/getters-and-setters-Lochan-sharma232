70e8c098f3052bbfdbf939dcf55e8dd911480276
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-21 23:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,13 +33,8 @@ class Teacher extends Person {
       console.log(this.name+"is teaching");
 	}
 }
-//const Person =new Person(name,age);
-const Person=new Person("John","25");
-const Student=new Student("Bob","40");
-console.log(Person.());
-console.log(Student.study());
-console.log(Teacher.teach())
-// Do not change the code below this line
+
+ //donot change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;

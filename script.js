b9efd3921c6b44ffbc5afbfef286669054e822c4
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-22 23:27:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ class Person {
 	get name(){
 		return this._name;
 	}
-   set age(age){
-		return this._age;
-	}
+   set age(age){//set the age
+	   this._age=age;
+	 	}
 	get age(){
 		return this._age;
 	}
 class Student extends Person {
 	study(){
-		console.log(`${this.name}+is studying`);
+		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 		teach(){
-      console.log(`${this.name}+is teaching`);
+      console.log(`${this.name} is teaching`);
 	}
 }
 
